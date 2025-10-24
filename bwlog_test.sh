8c8cfe0956381527c01fc3034e404d6cc3304734
Amal Krishna <66444212+AmalKrishna1@users.noreply.github.com>
2025-10-24 12:20:58 -0700

    Added automated testing to ensure simulated network traffic is actually written as numbers into the log file, still trying to figure out how to test the terminal output with last line in the log file

--- bwlog_test.sh
+++ bwlog_test.sh
@@ -22,6 +22,32 @@ teardown(){
   run bash bwlog.sh &
   sleep 5
   # check if the log file has data written from the simulated traffic
-  [-s "$LOG_FILE" ]
+  [ -s "$LOG_FILE" ]
   kill $!
+}
+
+@test "LOG FILE simulated traffic data are valid numbers" {
+  run bash bwlog.sh &
+  sleep 15
+
+  # -q = suppressed output to the terminal, so no unnecessary output is shown
+  # -v = invert the match, so we are looking for lines that do NOT match the pattern
+  # -E = use extended regular expressions
+  # [^0-9]+$ = ^ represents start of the line, 0 - 9 represents any digit, + means one or more of the preceding element, $ represents end of the line
+  # (\.[0-9]+)?$ = matches optional decimal point followed by one or more digits
+  # -qvE = combining the logic above, this command checks if there are any lines in the LOG_FILE that do NOT consist solely of digits (0-9)
+  if grep -qvE '^[0-9]+(\.[0-9]+)?$' "$LOG_FILE"; then
+    kill $!
+    echo "LOG FILE contains invalid traffic data"
+  fi
+
+  kill $!
+}
+
+@test "Output to the terminal is in HH:MM:SS: current traffix = XXXX KB/s format" {
+  run bash bwlog.sh &
+  sleep 65
+  
+  #Fill In the rest
+
 }
