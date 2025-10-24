13ee55b818c7fa5052a41d72ebb95d0a92d4ecf0
Amal Krishna <66444212+AmalKrishna1@users.noreply.github.com>
2025-10-23 18:29:14 -0700

    Automated testing for the log file and checking if information is actually written into the log file

--- bwlog_test.sh
+++ bwlog_test.sh
@@ -1,1 +1,27 @@
-#!usr/bin/env bats
+#!/usr/bin/env bats
+
+setup(){
+  export LOG_FILE="/tmp/bw.log"
+}
+
+teardown(){
+  rm -f "$LOG_FILE"
+}
+
+@test "LOG FILE is successfully created" { 
+  # run the bwlog.sh script in the background
+  run bash bwlog.sh &
+  # time to creat the log file
+  sleep 5
+  # check if the log file exists
+  [ -f "$LOG_FILE" ]
+  kill $!
+}
+
+@test "LOG FILE has written traffic data" {
+  run bash bwlog.sh &
+  sleep 5
+  # check if the log file has data written from the simulated traffic
+  [-s "$LOG_FILE" ]
+  kill $!
+}
