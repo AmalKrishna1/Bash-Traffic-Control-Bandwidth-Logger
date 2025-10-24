ea33e125e42bb6d3f42b992c88498bd0f2d84636
Amal Krishna <66444212+AmalKrishna1@users.noreply.github.com>
2025-10-23 17:40:59 -0700

    Generate Traffic Function using Real Netowkr Data - Output is Disfunctional

--- bwlog.sh
+++ bwlog.sh
@@ -7,7 +7,10 @@ LOG_FILE="/tmp/bw.log"
 gen_traffic(){
   while true; do
     # Get the current network traffic in KB/s
-    TRAFFIC=$(ifstat -i en0 1 1 | awk 'NR==3){print $1}')
+    #ifstat command is used to get the network traffic
+    # The interface 'en0' is based on system "MAC"
+    # 1 1 means to sample once every second for one time
+    TRAFFIC=$(ifstat -i en0 1 1 | awk 'NR==3 {print $1}')
     echo "$TRAFFIC" >> "$LOG_FILE"
     sleep 1
   done
@@ -18,3 +21,20 @@ file_cleanup(){
   rm -f "$LOG_FILE"
   exit 0
 }
+
+# Trap SIGINT and SIGTERM to clean up the log file
+trap file_cleanup SIGINT SIGTERM
+
+# Start generating traffic in the background
+gen_traffic &
+GEN_PID=$!
+
+for i in {1..6}; do
+  sleep 10
+  TRAFFIC=$(tail -n 1 "$LOG_FILE")
+  TIMESTAMP=$(date "+%H:%M:%S")
+  printf "%s: current traffic = %s KB/s \n" "$TIMESTAMP" "$TRAFFIC"
+done
+
+kill $GEN_PID
+file_cleanup
